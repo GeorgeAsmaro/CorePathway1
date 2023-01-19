b829118ef18e71fe6c31875d91121b5a33fcf1fc
George Asmaro <g.asmaro@surreyschools.ca>
2023-01-19 10:50:23 -0800

    saving in case of crash

--- CorePathway1/Assets/Scripts/ArrowMover.cs
+++ CorePathway1/Assets/Scripts/ArrowMover.cs
@@ -6,6 +6,7 @@ public class ArrowMover : MonoBehaviour
 {
     public float speed; 
     public GameObject arrow;
+    public float delay;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +16,9 @@ public class ArrowMover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position += (new Vector3(1,0,0) * Time.deltaTime * speed);
+        delay -= Time.deltaTime;
+        if(delay <=0 ) {
+            transform.position += (new Vector3(1,0,0) * Time.deltaTime * speed);
+        }
     }
 }
