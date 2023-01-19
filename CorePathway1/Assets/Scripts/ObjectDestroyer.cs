9ada951460047539166e64c422fea81c466f4e63
George Asmaro <g.asmaro@surreyschools.ca>
2023-01-19 10:53:05 -0800

    ./

--- CorePathway1/Assets/Scripts/ObjectDestroyer.cs
+++ CorePathway1/Assets/Scripts/ObjectDestroyer.cs
@@ -7,14 +7,7 @@ public class ObjectDestroyer : MonoBehaviour
     // Start is called before the first frame update
 
     public GameObject cube;
-    void Start() {
-        InvokeRepeating("Spawner", 0f, 2f);
-    }
-
-    void Spawner() {
-        GameObject tempCube = Instantiate(cube,transform.position, Quaternion.Euler(90, 0, -90));
-    }
-    // Update is called once per frame
+    
     void OnCollisionEnter(Collision hit)
     {
         
